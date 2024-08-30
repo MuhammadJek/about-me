9985e88cd6b9acbc40fee8c2946fa4bd1ad6161b
MuhammadDzakiFawwazArdika <118704683+MuhammadDzakiFawwazArdika@users.noreply.github.com>
2024-08-30 14:02:41 +0700

    revisi portofolio

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function toggleMobileMenu(menu){
-	menu.classList.toggle('open')
-	console.log('button berhasil');
+function toggleMobileMenu(menu) {
+  menu.classList.toggle("open");
+  console.log("button berhasil");
 }
+
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function () {
+  let currentScrollpos = window.pageYOffset;
+
+  if (prevScrollpos > currentScrollpos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-120px";
+  }
+  prevScrollpos = currentScrollpos;
+};
